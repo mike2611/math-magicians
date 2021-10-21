c622fd88d090cc50eb00f66af892190a778d9775
mike2611 <mpuentes2611@gmail.com>
2021-10-21 14:30:03 -0500

    Refactor App.js from class-based component to function based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import * as React from 'react';
 import Calculator from './components/Calculator';
 
-export default class App extends React.PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }

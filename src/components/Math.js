74cf8e731792ee11d478bf577c2c9eecc68fcfc6
mike2611 <mpuentes2611@gmail.com>
2021-10-26 22:56:25 -0500

    Fix styles for mobile

--- src/components/Math.js
+++ src/components/Math.js
@@ -2,7 +2,7 @@ import Calculator from './Calculator';
 import './Math.css';
 
 const Math = () => (
-  <div className="math d-flex">
+  <div className="math">
     <h1>Let&apos;s do some math!</h1>
     <Calculator />
   </div>

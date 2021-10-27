51e52d4a7e1dcf2079ac17cfab7faf1710e4acc6
mike2611 <mpuentes2611@gmail.com>
2021-10-26 22:42:11 -0500

    Add Quote styles

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,15 @@
+import './Quote.css';
+
 const Quote = () => (
-  <div>
-    <h1>
-      Mathematics is not about numbers, equations, computations,
-      or algorithms: it is about understanding. -William Pault Thurston
-    </h1>
+  <div className="quote">
+    <div>
+      <h1>
+        Mathematics is not about numbers, equations,
+        computations, or algorithms: it is about understanding.
+        <br />
+        <span className="author">-William Pault Thurston</span>
+      </h1>
+    </div>
   </div>
 );
 

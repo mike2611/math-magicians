05601c1932e74ea799a46b2d97ba6d0392d2866f
mike2611 <mpuentes2611@gmail.com>
2021-10-26 20:31:49 -0500

    Add Quote.js file

---
+++ src/components/Quote.js
@@ -0,0 +1,10 @@
+const Quote = () => (
+  <div>
+    <h1>
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: it is about understanding. -William Pault Thurston
+    </h1>
+  </div>
+);
+
+export default (Quote);

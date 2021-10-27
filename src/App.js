8009fd3ae8ee4ba86e279bc58b8a1aafa33d0919
mike2611 <mpuentes2611@gmail.com>
2021-10-26 20:32:10 -0500

    Add Math.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Calculator from './components/Calculator';
 import Home from './components/Home';
+import Quote from './components/Quote';
+import Math from './components/Math';
 
 const App = () => (
   <div className="App">
+    <Quote />
     <Home />
-    <Calculator />
+    <Math />
   </div>
 );
 
---
+++ src/components/Math.js
@@ -0,0 +1,10 @@
+import Calculator from './Calculator';
+
+const Math = () => (
+  <div>
+    <h1>Let&apos;s do some math!</h1>
+    <Calculator />
+  </div>
+);
+
+export default (Math);

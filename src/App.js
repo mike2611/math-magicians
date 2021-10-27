dadd3b43169e8d36fed477deb7f38d68e525ec17
mike2611 <mpuentes2611@gmail.com>
2021-10-26 20:49:00 -0500

    Add Router to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import {
+  BrowserRouter as Router, Link, Switch, Route,
+} from 'react-router-dom';
+
 import Home from './components/Home';
 import Quote from './components/Quote';
 import Math from './components/Math';
 
 const App = () => (
-  <div className="App">
-    <Quote />
-    <Home />
-    <Math />
-  </div>
+  <Router>
+    <div className="App">
+      <h1>Math Magicians</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>|</li>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li>|</li>
+        <li>
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/calculator">
+          <Math />
+        </Route>
+        <Route path="/quote">
+          <Quote />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
